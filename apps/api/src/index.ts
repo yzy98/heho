621badb0e98f3eed9b1ff7ecbf8726067b1e9cfa
yzy98 <yz186337@163.com>
2026-06-18 12:09:20 +0800

    feat: scaffold Hono API server

---
+++ apps/api/src/index.ts
@@ -0,0 +1,16 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => c.text("Hello Hono!"));
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 4000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
